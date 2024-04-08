b342a47d1320bed8f3953e2e84ec30d5a5752c3f
Kirill <kma152@sfu.ca>
2024-04-07 19:08:39 -0700

    data cleaned and prepared for the model usage

---
+++ prepare_data.py
@@ -0,0 +1,12 @@
+# remove useless colomns, modify some features and prepare data file for usage by models
+import pandas as pd
+
+data = pd.read_csv('C:/Users/kiril/Desktop/CMPT/353/final project/CMPT353-Project/modified_normalized_data.csv')
+
+data = data.drop(columns=['type','nameOrig','nameDest'])
+columns_to_replace = ['type_CASH_IN', 'type_CASH_OUT', 'type_DEBIT', 'type_PAYMENT', 'type_TRANSFER']
+data[columns_to_replace] = data[columns_to_replace].replace({True:1,False:0})
+isFraud_col = data.pop('isFraud')
+data['isFraud'] = isFraud_col
+
+data.to_csv('clean_data.csv',index=False)
