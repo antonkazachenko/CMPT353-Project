eb855d860b9a2ef27fc5ecf324207bde597f68b9
Damir <dza62@sfu.ca>
2024-04-09 22:22:51 -0700

    Initial commit. Implementation of boost xgb.

--- Random_Forest params.py
+++ Random_Forest params.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
-
+import xgboost as xgb
 
 data = pd.read_csv('clean_data.csv')
 
